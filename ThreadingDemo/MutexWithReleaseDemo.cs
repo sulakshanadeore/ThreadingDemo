78cda7f1654600751d52f1e4f48d2043fbcbb844
deoresulakshana@gmail.com <36725147+sulakshanadeore@users.noreply.github.com>
2022-09-09 12:57:09 +0530

    Add project files.

---
+++ ThreadingDemo/MutexWithReleaseDemo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadingDemo
+{
+    class MutexWithReleaseDemo
+    {
+        private Mutex mutex = new Mutex();
+        const int limits = 1;
+        const int threads = 4;
+
+        static void ThreadsDemo()
+        {
+            for (int i = 0; i < limits; i++)
+            {
+                UseCode();
+            }
+        }
+        static void UseCode()
+        {
+            mutex.WaitOne();
+            Console.WriteLine("Use code has  goig to "    );
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
